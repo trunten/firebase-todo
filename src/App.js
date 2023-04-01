3e83a18e896509a20454c6dfc5b57e81a8a75ccf
Jonathan Davis <trunten@btinternet.com>
2023-04-01 19:11:04 +0100

    --allow-empty

--- src/App.js
+++ src/App.js
@@ -95,7 +95,7 @@ function TodoList() {
               : <div key={todo.id} style={{...flex, paddingBlock: "20px", borderTop:"1px solid grey"}}>
                   <li style={{...flex, width:"100%", color: todo.complete ? "grey": "inherit"}}>{todo.text}</li>
                   {todo.complete 
-                    ? <div style={{minWidth:"150px"}}></div> 
+                    ? <div style={{minWidth:"80px"}}></div> 
                     : <button onClick={()=>completed(todo.id)} style={{maxWidth:"80px", border:"none", margin:0, backgroundColor:"green"}}>
                         <svg stroke="currentColor" fill="currentColor" stroke-width="0" version="1.1" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 2l-7.5 7.5-3.5-3.5-2.5 2.5 6 6 10-10z"></path></svg>
                       </button>
